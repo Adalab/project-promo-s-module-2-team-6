cf50c13482693d18b75cf0d9af2c60c392c86a23
celsrami <celsrami22@gmail.com>
2023-02-08 13:43:43 +0100

    js collapsed

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,66 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+
+// llamar legend
+// llamar a los div
+
+const designLegend = document.querySelector('.js-designLegend');
+const divDesign = document.querySelector('.js-divDesign');
+
+const fillLegend = document.querySelector('.js-fillLegend');
+const divFill = document.querySelector('.js-divFill');
+
+
+const shareLegend = document.querySelector('.js-shareLegend');
+const divShare = document.querySelector('.js-divShare');
+
+// crear clase collapsed
+
+function closeAlldiv () {
+divDesign.classList.add ('collapsed');
+divFill.classList.add ('collapsed');
+divShare.classList.add ('collapsed');
+}
+
+function openDesign () {
+    divDesign.classList.remove('collapsed');
+}
+
+function openFill () {
+    divFill.classList.remove('collapsed');
+}
+
+function openShare () {
+    divShare.classList.remove('collapsed');
+}
+
+// Evento Design
+
+function handleClikDesign (event) {
+    console.log('click');
+    closeAlldiv ();
+    openDesign () ;
+}
+
+designLegend.addEventListener ('click', handleClikDesign);
+
+// Evento Fill
+
+function handleClikFill (event) {
+    console.log('click');
+    closeAlldiv ();
+    openFill () ;
+}
+
+fillLegend.addEventListener ('click', handleClikFill);
+
+// Evento Fill
+
+function handleClikShare (event) {
+    console.log('click');
+    closeAlldiv ();
+    openShare () ;
+}
+
+shareLegend.addEventListener ('click', handleClikShare);

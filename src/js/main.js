346b423b0e5c98e18c3f219562cdd5142f66038b
Lurdesmg <lurdesxi07@gmail.com>
2023-02-10 13:17:50 +0100

    add-event JS

--- src/js/main.js
+++ src/js/main.js
@@ -68,4 +68,61 @@ function handleClikShare(event) {
 
 shareLegend.addEventListener('click', handleClikShare);
 
+// Valores del input
 
+const inputName = document.querySelector('.js-inputName');
+const cardName = document.querySelector('.js-cardName');
+const inputJob = document.querySelector('.js-inputJob');
+const cardJob = document.querySelector('.js-cardJob');
+const inputEmail = document.querySelector('.js-inputEmail');
+const cardEmail = document.querySelector('.js-cardEmail');
+const inputPhone = document.querySelector('.js-inputPhone');
+const cardPhone = document.querySelector('.js-cardPhone');
+const inputLinkedin = document.querySelector('.js-inputLinkedin');
+const cardLinkedin = document.querySelector('.js-cardLinkedin');
+const inputGithub = document.querySelector('.js-inputGithub');
+const cardGithub = document.querySelector('.js-cardGithub');
+
+// Evento Nombre
+inputName.addEventListener('keyup' , () => {
+    const nameWritten = inputName.value;
+    if (nameWritten === '') {
+        cardName.innerHTML = 'Nombre Apellido';
+    } else {
+        cardName.innerHTML = inputName.value;
+    }
+});
+// Evento Empleo
+inputJob.addEventListener('keyup' , () => {
+    const jobWritten = inputJob.value;
+    if (jobWritten === '') {
+        cardJob.innerHTML = 'Front-end-developer';
+    } else {
+        cardJob.innerHTML = inputJob.value;
+    }
+});
+
+// Evento Email
+inputEmail.addEventListener('keyup' , () => {
+    const emailWritten = inputEmail.value;
+    cardEmail.href = `mailto:${emailWritten}`;
+});
+
+//Evento Linkedin
+inputLinkedin.addEventListener('keyup' , () => {
+    const linkedinWritten = inputLinkedin.value;
+    cardLinkedin.href = `${linkedinWritten}`;
+});
+
+// Evento github
+inputGithub.addEventListener('keyup' , () => {
+    const githubWritten = inputGithub.value;
+    cardGithub.href = `https://github.com/${githubWritten.slice(1 , 50)}`;
+    console.log('hola');
+});
+
+// Evento telefono
+inputPhone.addEventListener('keyup' , () => {
+    const phoneWritten = inputPhone.value;
+    cardPhone.href = `tel:${phoneWritten}`;
+});

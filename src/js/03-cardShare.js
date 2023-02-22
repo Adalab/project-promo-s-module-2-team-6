ff9fb7bc6f98cf8cae454e1ea0e2b6698a26f6d6
celsrami <celsrami22@gmail.com>
2023-02-22 17:17:24 +0100

    msj error

--- src/js/03-cardShare.js
+++ src/js/03-cardShare.js
@@ -33,7 +33,7 @@ function dataFetch() {
       console.log(data);
 
       if (data.success === false) {
-        msjError.innerHTML = '*  Te faltan datos por rellenar '
+        msjError.innerHTML = '* Te faltan datos por rellenar ';
       } else {
         msjError.innerHTML = '';
         buttonCard.classList.add('collapsed');
